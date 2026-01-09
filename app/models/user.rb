726a63f15c650a0f5261bda5847c096e3741ffed
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-01-09 12:34:22 +0900

    アカウント名にバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, uniqueness: true
 end
