4bcf748d07a77b736e061f1d76e775436c404bd4
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-01-13 14:15:23 +0900

    webpackの設定

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,37 @@ const path    = require("path")
 const webpack = require("webpack")
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   devtool: "source-map",
-  entry: {
+
+  entry: {  // エントリポイント
     application: "./app/javascript/application.js"
   },
+
   output: {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     chunkFormat: "module",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
+
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
     })
-  ]
+  ],
+
+  resolve: {
+
+    modules: [
+      path.resolve(__dirname, 'app/javascript'), // 1. アプリケーションの JS ルート
+      'node_modules'                             // 2. それで見つからなければ node_modules
+    ],
+
+    alias: {  // インポートを簡潔にするためのエイリアス
+      jquery: path.resolve(__dirname, 'node_modules/jquery'),
+      },
+
+    symlinks: false  // symlinkを辿らない（WSL/Windows間のパス混乱防止）
+  },
 }
