df9cfe8a61f43b18f9efb014c1de90b94a501607
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-01-16 18:12:41 +0900

    記事一覧を表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create ]
 
   def index
+    @articles = Article.order(created_at: :desc)
   end
 
   def new
