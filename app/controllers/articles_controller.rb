f22dd71e31cac534210a24c2c7838b050c0a0f94
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-01-16 14:02:53 +0900

    newアクション

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [ :new ]
+
   def index
+  end
+
+  def new
+    @article = current_user.articles.build
   end
 end
