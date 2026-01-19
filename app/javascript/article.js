acbc9768df1d01f71ab3cf427c70a370ebbf1ff3
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-01-19 17:10:17 +0900

    ハートの表示をコントロールするfuncitonを定義

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -4,25 +4,30 @@ import Rails from "@rails/ujs"
 
 axios.defaults.headers.common['X-CSRF-Token'] = Rails.csrfToken()
 
+const handleHeartDisplay = (el, hasLiked) => {
+
+  const activeHeart = $(el).find('.active-heart')
+  const inactiveHeart = $(el).find('.inactive-heart')
+
+  if (hasLiked) {
+    activeHeart.removeClass('hidden')
+  } else {
+    inactiveHeart.removeClass('hidden')
+  }
+}
+
 document.addEventListener('turbo:load', () => {
 
   // いいねの状態を取得してハートを表示
   $('.article').each(function() {
 
+    const self = this
     const articleId = $(this).data('articleId')
 
     axios.get(`/articles/${articleId}/like`)
     .then((res) => {
-
       const hasLiked = res.data.hasLiked
-      const activeHeart = $(this).find('.active-heart')
-      const inactiveHeart = $(this).find('.inactive-heart')
-
-      if (hasLiked) {
-        activeHeart.removeClass('hidden')
-      } else {
-        inactiveHeart.removeClass('hidden')
-      }
+      handleHeartDisplay(self, hasLiked)
     })
   })
 
