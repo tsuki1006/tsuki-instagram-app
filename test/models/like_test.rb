f41d3d6d6ee437d35d6391dce21037eb886d2e14
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-01-20 16:07:30 +0900

    rubocop

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -18,7 +18,7 @@
 #  fk_rails_...  (article_id => articles.id)
 #  fk_rails_...  (user_id => users.id)
 #
-require "test_helper"
+require 'test_helper'
 
 class LikeTest < ActiveSupport::TestCase
   # test "the truth" do
