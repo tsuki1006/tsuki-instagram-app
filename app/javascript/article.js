bc3ece79a007a4f7175de41608e6253de4c9ffc1
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-01-20 15:57:10 +0900

    ハートの処理を別ファイルに

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -1,6 +1,10 @@
 import $ from 'jquery'
 import axios from 'axios'
 import Rails from "@rails/ujs"
+import {
+  listenInactiveHeartEvent,
+  listenActiveHeartEvent
+} from 'modules/handle_heart'
 
 axios.defaults.headers.common['X-CSRF-Token'] = Rails.csrfToken()
 
@@ -32,41 +36,8 @@ document.addEventListener('turbo:load', () => {
   })
 
   // いいねする
-  $('.inactive-heart').on('click', function() {
-    const targetArticle = $(this).closest('.article')
-    const activeHeart = targetArticle.find('.active-heart')
-    const articleId = targetArticle.data('articleId')
-
-    axios.post(`/articles/${articleId}/like`)
-      .then((res) => {
-        if (res.data.status === 'ok') {
-          $(this).addClass('hidden')
-          activeHeart.removeClass('hidden')
-        }
-      })
-      .catch((e) => {
-        window.alert('Error')
-        console.log(e)
-      })
-  })
-
+  listenInactiveHeartEvent()
   // いいねを外す
-  $('.active-heart').on('click', function() {
-    const targetArticle = $(this).closest('.article')
-    const inactiveHeart = targetArticle.find('.inactive-heart')
-    const articleId = targetArticle.data('articleId')
-
-    axios.delete(`/articles/${articleId}/like`)
-      .then((res) => {
-        if (res.data.status === 'ok') {
-          $(this).addClass('hidden')
-          inactiveHeart.removeClass('hidden')
-        }
-      })
-      .catch((e) => {
-        window.alert('Error')
-        console.log(e)
-      })
-  })
+  listenActiveHeartEvent()
 
 })
