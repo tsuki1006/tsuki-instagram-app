faa7bbdb6d498d637df68b5c96f9d18ec4bb4175
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-01-22 17:37:44 +0900

    引数の名称を変更（user → mentioned_user）

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,7 +1,7 @@
 class CommentMailer < ApplicationMailer
-  def mentioned(user, comment)
-    @user = user
+  def mentioned(mentioned_user, comment)
+    @mentioned_user = mentioned_user
     @article = Article.find(comment.article_id)
-    mail to: user.email , subject: '【お知らせ】メンションされました'
+    mail to: @mentioned_user.email , subject: '【お知らせ】メンションされました'
   end
 end
