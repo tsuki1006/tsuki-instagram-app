fcf5f1f18c39eee32f5ed51913abd31767e0d005
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2026-01-28 14:55:43 +0900

    フォロー / フォロワーの関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,11 +31,26 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+
+  has_many :followings, through: :following_relationships, source: :following
+  has_many :followers, through: :follower_relationships, source: :follower
+
   def has_liked?(article)
     likes.exists?(article_id: article.id)
   end
 
   def prepare_profile
     profile || build_profile
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
   end
 end
